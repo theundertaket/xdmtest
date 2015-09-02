972dd8ce1013ef0279211e90ad6f32ab749cfe88
xdm <563330858@qq.com>
2015-09-02 15:43:50 -0700

    初始化项目

---
+++ Administrivia/Administrivia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Administrivia
+//
+//  Created by ios on 15-9-2.
+//  Copyright (c) 2015年 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
